534cf3d68710957086487db3e8c7595b39fbc17b
rajdeep-engineer <rajdeepdas1986@gmail.com>
2024-01-08 12:39:50 +0530

    updated communication protocol

--- communication.py
+++ communication.py
@@ -1,13 +1,25 @@
 import serial
 
-ser = serial.Serial('', 115200)
+ser = serial.Serial()
 
 
+# to list ports run the following command in shell
+# python3 -m serial.tools.list_ports
+
+def connect_ethercat():
+    ser.baudrate = 115200
+    ser.port = '/dev/tty/ACM0'
+    ser.open()
+
+
+# Format: XXXnnn => XXX: data ref; nnn: data value; 6 bytes of data
 def read_data():
-    data_in = ser.readline()
+    data_in = ser.read(6)
     return data_in
 
 
+# Format: Xxxnnn => X: zone ref (A, B, C, D, E); xx: button number (1-99); nnn: value (0 to 100)
+# return value should be 6 bytes
 def write_data(data_out):
-    ser.write(data_out)
-    return
+    ret = ser.write(data_out)
+    return ret
